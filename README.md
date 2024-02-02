1d52226e7f0794f2234278a59b5ffe68a9ed7083
adityatlgt <adityatlgt@gmail.com>
2024-02-02 12:00:29 +0530

    updates

---
+++ README.md
@@ -0,0 +1,29 @@
+# About:
+ It is an ecommerce app created using python(fast-api).
+
+
+## Requirements:
+Requirments are available in requirements.txt file
+
+# Installation
+#####   Install the requirements.
+##### Start the app using:
+    uvicorn main:app 
+
+## API Endpoints
+    /token (POST): Provides a bearer token for authentication.
+    /user_data (POST): Provides user data.
+    /users/ (GET): Retrieves a list of users.
+    /business/{id} (PUT): Updates business data.
+    /user_register/ (POST): Registers a new user.
+    /uploadfile/profile (POST): Uploads a profile image.
+    /uploadfile/product/{id} (POST): Uploads a product image.
+    /products/ (POST): Adds a new product.
+    /products/{id} (DELETE): Deletes a product.
+    /products/{id} (PUT): Updates a product.
+    /products (GET): Retrieves a list of products.
+    /products/{id} (GET): Retrieves details of a specific product.
+    Authentication
+
+## Authentication used:
+    Token Authentication
---
+++ emails.py
@@ -0,0 +1,73 @@
+from typing import List
+
+from models import User
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from pydantic import EmailStr
+import jwt
+from config import get_settings
+
+SITE_URL = get_settings().SITE_URL
+SITE_NAME = get_settings().SITE_NAME
+
+"""
+EMAIL configurations
+"""
+conf = ConnectionConfig(
+    MAIL_USERNAME=get_settings().MAIL_USERNAME,
+    MAIL_PASSWORD=get_settings().MAIL_PASSWORD,
+    MAIL_FROM=get_settings().MAIL_FROM,
+    MAIL_PORT=get_settings().MAIL_PORT,
+    MAIL_SERVER=get_settings().MAIL_SERVER,
+    MAIL_TLS=get_settings().MAIL_TLS,
+    MAIL_SSL=get_settings().MAIL_SSL,
+    USE_CREDENTIALS=get_settings().USE_CREDENTIALS,
+    VALIDATE_CERTS=get_settings().VALIDATE_CERTS
+)
+
+"""
+This function is used to send verification email to the user.
+"""
+async def send_mail(email: List[EmailStr], instance: User):
+
+    token_data = {
+        "id": instance.id,
+        "username": instance.username,
+        "email": instance.email
+    }
+
+    token = jwt.encode(token_data, get_settings().SECRET, algorithm="HS256")
+
+    template = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div style = "display:flex; align-items: center; flex-direction: column" >
+            <h3>Account Verification</H3>
+
+            <br>
+
+            <p>
+                Thanx for choosing us, please click on the button below
+                to verify your account
+            </p> 
+            
+            <a style = "display:marign-top: 1rem ; padding: 1rem; border-redius: 0.5rem;
+             font-size:1rem; text-decoration: no; background: #0275d8; color:white"
+             href="{SITE_URL}/verification/email/?token={token}">
+                Verify your email
+             </a>
+        </div>
+    </body>
+    </html>
+    """
+    message = MessageSchema(
+        subject=SITE_NAME + " account verification",
+        recipients=email, 
+        body=template,
+        subtype="html"
+    )
+    fm = FastMail(conf)
+    await fm.send_message(message)
